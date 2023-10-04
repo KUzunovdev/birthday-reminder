2a7d86a04dfd84b8ab83ee28d415afc64e085b3c
Ki11akrokBg <kikouzunow@gmail.com>
2023-10-04 13:47:42 +0300

    feat: Add logout logic

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,84 +1,97 @@
 import React, { useEffect } from "react";
 import BirthdayContainer from "./components/BirthdayContainer";
-import {auth ,db} from '../server/firebaseConfig';
+import { auth, db } from "../server/firebaseConfig";
 import "../styles/Main.css";
 import { useState } from "react";
 import { collection, getDocs } from "firebase/firestore";
 import AddPopUp from "./components/AddPopUp";
+import { useNavigate } from "react-router-dom";
 
 // setBirthdayList(birthdayList.docs.map(doc => doc.data()));
 // const data = await db.collection('birthdays').get();
 const Main = () => {
-
-
-  const[birthdayList, setBirthdayList] = useState([]);
+  const navigate = useNavigate();
+  const [birthdayList, setBirthdayList] = useState([]);
   const [showAddPopUp, setShowAddPopUp] = useState(false);
 
   const birthdayRef = collection(db, "birthdays");
 
-  useEffect(() => {
-    const getBirthdaysList = async () => {
-
-      try{
-      const data = await getDocs(birthdayRef);  
+  const getBirthdaysList = async () => {
+    try {
+      const data = await getDocs(birthdayRef);
       const filteredData = data.docs.map((docs) => ({
         ...docs.data(),
         id: docs.id,
       }));
       setBirthdayList(filteredData);
-      console.log(filteredData);                        
-      } catch (err) {
-        console.log(err);
-      } 
-    };
+      console.log(filteredData);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
+  useEffect(() => {
     getBirthdaysList();
   }, []);
 
-  
-  const[birthdayCount, setBirthdayCount] = useState(0);
+  const [birthdayCount, setBirthdayCount] = useState(0);
 
-  const userProfilePicture = auth.currentUser?.providerData[0]?.providerId === "google.com" && auth.currentUser.photoURL
-    ? auth.currentUser.photoURL
-    : '../assets/default_profile_picture.png';
+  const signOut = async () => {
+    try {
+      await auth.signOut();
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-    const userProfileName = auth.currentUser?.providerData[0]?.providerId === "google.com" && auth.currentUser.displayName
-    ? auth.currentUser.displayName
-    : 'User';
+  const userProfilePicture =
+    auth.currentUser?.providerData[0]?.providerId === "google.com" &&
+    auth.currentUser.photoURL
+      ? auth.currentUser.photoURL
+      : "../assets/default_profile_picture.png";
+
+  const userProfileName =
+    auth.currentUser?.providerData[0]?.providerId === "google.com" &&
+    auth.currentUser.displayName
+      ? auth.currentUser.displayName
+      : "User";
 
   return (
-
-    
-
     <div>
       {/* Maybe use mui-material*/}
-          <div className="profile-wrapper">
-            <img src={userProfilePicture} alt=""/>
-            <p>{userProfileName}</p>
-          </div>
+      <div className="profile-wrapper">
+        <img src={userProfilePicture} alt="" />
+        <p>{userProfileName}</p>
+        <button onClick={signOut}>Log out</button>
+      </div>
 
-          <p className="birthday-count">{birthdayCount} birthdays today</p>
-          <hr></hr>
+      <p className="birthday-count">{birthdayCount} birthdays today</p>
+      <hr></hr>
 
-        {/* Add/ Remove BirthdayContainers*/}
-        <div className="birthday-wrapper">
-          {birthdayList.map((birthday) => (
+      {/* Add/ Remove BirthdayContainers*/}
+      <div className="birthday-wrapper">
+        {birthdayList.map((birthday) => (
           <BirthdayContainer
             key={birthday.id}
             name={birthday.name}
             img={birthday.imgURL}
             date={birthday.birthDate}
-            />
-          ))}
+          />
+        ))}
 
-          {/*Conditional rendering of AddPopUp*/}
-          {showAddPopUp && <AddPopUp />}
-         
-         </div>
+        {/*Conditional rendering of AddPopUp*/}
+        {showAddPopUp && <AddPopUp />}
+      </div>
 
-         <div className="button-plus" onClick={()=> {setShowAddPopUp(!showAddPopUp)}}></div>
+      <div
+        className="button-plus"
+        onClick={() => {
+          setShowAddPopUp(!showAddPopUp);
+        }}
+      ></div>
     </div>
   );
-}
+};
 
 export default Main;
